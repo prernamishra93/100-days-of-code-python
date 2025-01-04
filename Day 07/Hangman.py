91f0105f835a089cb7b1f7bb963f0638e1385377
Prerna Mishra <prernamishra@Prernas-MacBook-Air.local>
2025-01-04 23:02:02 +0530

    Update

--- Day 07/Hangman.py
+++ Day 07/Hangman.py
@@ -9,18 +9,23 @@ print(chosen_word)
 placeholder = ''
 for letter in chosen_word:
     placeholder += '_'
-print(placeholder)
+print(f'Word to guess : {placeholder}')
 
 game_over = False
 correct_letter = []
 
 lives = 6
-
 while not game_over:
+    print(f'***************************{lives}/6 LIVES LEFT ***************************')
     guess = input('\nTo win, guess the word before the person is hung.\n').lower()
+
+    if guess in correct_letter:
+        print(f'You\'v already guessed {guess}')
+    
     display = ''
     if guess not in chosen_word:
         lives -= 1
+        print(f"you guessed {guess}, that's not in the word. You lose a life.")
     
     for letter in chosen_word:
         if guess == letter:
@@ -36,7 +41,7 @@ while not game_over:
 
     if '_' not in display:
         game_over = True
-        print('You won!')
+        print('***************************YOU WIN***************************')
     elif lives == 0:
         game_over = True
-        print('You lose!')
+        print(f'***************************IT WAS {chosen_word}! YOU LOSE***************************')
